54f74f884e6dc4fb8efc7047126cfe34bdbf4a99
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-01-31 22:59:46 -0300

    feat: filling the days

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -1,6 +1,11 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
 import { HabitDay } from "./HabitDay";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const summaryDates = generateDatesFromYearBeginning();
+const minimumSummaryDatesSize = 18 * 7; //18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
+
 export function SummaryTable() {
   return (
     <div className="w-full flex">
@@ -13,13 +18,13 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
-        <HabitDay />
+        {summaryDates.map((date) => (
+          <HabitDay key={date.toString()} />
+        ))}
+
+        {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+          <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
+        ))}
       </div>
     </div>
   )
