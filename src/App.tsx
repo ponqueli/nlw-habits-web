bdbb9ec35ac1375b646de396a58cf8ac7ed94b3e
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-01-31 22:18:22 -0300

    feat: component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
+import { Header } from './components/Header';
 import './styles/global.css';
-import { Habit } from "./components/Habit"
+//import { Habit } from "./components/Habit"
 
-function App() {
+export function App() {
   return (
-    <>
-      <Habit completed={3} />
-      <Habit completed={2} />
-      <Habit completed={0} />
-      <Habit completed={5} />
-      <Habit completed={10} />
-    </>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <Header />
+      </div>
+    </div>
   )
 }
-
-export default App
