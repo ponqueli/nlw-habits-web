3cb7ee1a11c09c4400e59804326a37da59284526
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-02-07 22:03:52 -0300

    feat: clsx lib for css and colorize the habits

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -18,9 +18,15 @@ export function SummaryTable() {
       </div>
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
-        {summaryDates.map((date) => (
-          <HabitDay key={date.toString()} />
-        ))}
+        {summaryDates.map((date) => {
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          )
+        })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => (
           <div key={index} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed" />
