d16a55a2dbff6dbad7ea3bc544678b600a03c00c
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2023-02-24 21:38:32 -0300

    feat: add focus on the buttons

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -98,7 +98,7 @@ export function NewHabitForm({ setOpen }: any) {
       </div>
       <button
         type="submit"
-        className='mt-6 rounded-lg p-4 flex items-center justify-center gap-3 bg-green-600 text-white font-semibold hover:bg-green-500 transition-colors duration-200'
+        className='mt-6 rounded-lg p-4 flex items-center justify-center gap-3 bg-green-600 text-white font-semibold hover:bg-green-500 transition-colors duration-200 focus: outline-none focus: ring-2 focus:ring-green-600 focus: ring-offset-2 focus: ring-offset-zinc-900'
       >
         <Check size={20} weight='bold' />
         Confirmar
